9e001f7e7ca48f6136c36dd5aebd7dd74c7667f3
Plamen Petrov <biznetmen@gmail.com>
2025-10-18 12:10:18 +0300

    Generate RAG Responses

--- index.ts
+++ index.ts
@@ -1,9 +1,21 @@
-import { runMemoryAgent } from "./agent";
+import { runMemoryAgent, runAiSupportAgent } from './agents';
 
 async function main() {
-    const query = "What Qwen LLM model can I run smoothly on my PC via Ollama?";
-    const chunks = await runMemoryAgent(query);
-    console.log("Retrieved Chunks:", chunks);
+	const query = 'What Qwen LLM model can I run smoothly on my PC via Ollama?';
+	const chunks = await runMemoryAgent(query);
+
+	const completion = await runAiSupportAgent({
+		chunks,
+		query,
+	});
+
+	console.log('Completion:', completion);
 }
+
+// async function main() {
+//     const query = "What Qwen LLM model can I run smoothly on my PC via Ollama?";
+//     const chunks = await runMemoryAgent(query);
+//     console.log("Retrieved Chunks:", chunks);
+// }
 
 main().catch(console.error);
